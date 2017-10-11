f7f86dc7a34358ad403562f5f78d93c8609e1dee
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-10-11 15:48:03 -0300

    Making menu a recursive function

--- hospitalSystem/src/main/java/view/HospitalSystem.java
+++ hospitalSystem/src/main/java/view/HospitalSystem.java
@@ -19,71 +19,75 @@ public class HospitalSystem {
 		
 		boolean success = hospitalDAO.addHospital(hospital);
 		
-		int option;
+		int option = 0;
 		
 		if (success) {
 			menu.mainMenu();
 			option = scanner.nextInt();
 		
-			switch (option) {
-			// WingsOfBuilding
-			case 1:
-				break;
-			// Nursery
-			case 2:
-				break;
-			// Laboratories
-			case 3:
-				break;
-			// Medical Agreements
-			case 4:
-				break;
-			// Medical Equipments
-			case 5:
-				break;
-			// Medical Exams
-			case 6:
-				break;
-			// Doctors
-			case 7:
-				break;
-			// Nurses
-			case 8:
-				break;
-			// Patients
-			case 9:
-				break;
-			// About the hospital
-			case 10:
-				view.clear();
-				hospitalDAO.getAllHospitals();
-				System.out.println("Deseja voltar ao menu (Sim / Não)?");
-				String back = scanner.next();
-				
-				if(menu.backToMenu(back)) {
+			do {
+				switch (option) {
+				// WingsOfBuilding
+				case 1:
+					break;
+				// Nursery
+				case 2:
+					break;
+				// Laboratories
+				case 3:
+					break;
+				// Medical Agreements
+				case 4:
+					break;
+				// Medical Equipments
+				case 5:
+					break;
+				// Medical Exams
+				case 6:
+					break;
+				// Doctors
+				case 7:
+					break;
+				// Nurses
+				case 8:
+					break;
+				// Patients
+				case 9:
+					break;
+				// About the hospital
+				case 10:
+					view.clear();
+					hospitalDAO.getAllHospitals();
+					System.out.println("Deseja voltar ao menu (Sim / Não)?");
+					String back = scanner.next();
+					
+					if(menu.backToMenu(back)) {
+						view.clear();
+						menu.mainMenu();
+						option = scanner.nextInt();
+						scanner.nextLine();
+					} else {
+						view.clear();
+						System.out.println("Fim da execução.");
+						
+						option = 11;
+					}
+					break;
+				case 11:
+					view.clear();
+					System.out.println("Fim da execução.");
+					break;
+				default:
+					view.clear();
+					System.out.println("Opção inválida! Tente novamente");
+					
 					view.clear();
 					menu.mainMenu();
 					option = scanner.nextInt();
 					scanner.nextLine();
-				} else {
-					view.clear();
-					System.out.println("Fim da execução.");
+					break;
 				}
-				break;
-			case 11:
-				view.clear();
-				System.out.println("Fim da execução.");
-				break;
-			default:
-				view.clear();
-				System.out.println("Opção inválida! Tente novamente");
-				
-				view.clear();
-				menu.mainMenu();
-				option = scanner.nextInt();
-				scanner.nextLine();
-				break;
-			}
+			} while (option != 11);
 		}
 	}
 	
